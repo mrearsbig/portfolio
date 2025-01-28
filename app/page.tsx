57170a77c2abab8e794aa6af7bd5d06c00083289
Alexander Viafara <mrearsbig@gmail.com>
2025-01-27 19:47:32 -0500

    Add about section

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,125 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import {
+  faFacebook,
+  faGithub,
+  faInstagram,
+  faLinkedin,
+  faXTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Briefcase, FolderClosed, UserRound } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="min-h-screen">
+      <div className="mx-auto min-h-screen lg:px-24 px-6 flex flex-col lg:flex-row gap-12 lg:gap-24">
+        {/* Left sidebar */}
+        <header className="lg:sticky lg:top-0 lg:max-h-screen lg:w-1/2 lg:flex lg:flex-col lg:justify-between lg:py-24">
+          <div>
+            <nav className="mb-16 hidden lg:block">
+              <ul className="flex gap-4">
+                <li>
+                  <Link href="#about">
+                    <Button variant="outline" size="sm">
+                      <UserRound />
+                      About
+                    </Button>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="#projects">
+                    <Button variant="outline" size="sm">
+                      <FolderClosed />
+                      Projects
+                    </Button>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="#experience">
+                    <Button variant="outline" size="sm">
+                      <Briefcase />
+                      Experience
+                    </Button>
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+            <h1 className="text-5xl font-bold mb-5">
+              <Link href="/">Alexander Viafara</Link>
+            </h1>
+            <h2 className="text-2xl mb-4">Software Engineer</h2>
+            <p className="text-lg">
+              More than a developer, a architect of the future.
+            </p>
+          </div>
+          {/* Social media */}
+          <div className="hidden lg:flex gap-4 items-center mt-8">
+            <Link
+              href="https://github.com/mrearsbig"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faGithub} className="h-6 w-6" />
+            </Link>
+            <Link
+              href="https://x.com/mrearsbig"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faXTwitter} className="h-6 w-6" />
+            </Link>
+            <Link
+              href="https://www.linkedin.com/in/mrearsbig"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faLinkedin} className="h-6 w-6" />
+            </Link>
+            <Link
+              href="https://www.instagram.com/mrearsbig"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faInstagram} className="h-6 w-6" />
+            </Link>
+            <Link
+              href="https://www.facebook.com/mrearsbig"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faFacebook} className="h-6 w-6" />
+            </Link>
+          </div>
+        </header>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        {/* Main content */}
+        <main className="lg:w-1/2 lg:py-24">
+          {/* About */}
+          <section className="mb-16 lg:mb-36">
+            <p className="text-lg mb-4">
+              I am Alexander Viafara, a passionate software developer and
+              empirical designer. My journey in the software world started more
+              than three years ago, and since then, I have developed a unique
+              ability to combine programming logic with visual design, always
+              aiming for the best user experience.
+            </p>
+            <p className="text-lg mb-4">
+              Throughout my career, I have worked on various projects, from
+              enterprise systems to mobile applications, always with a focus on
+              detail and usability. Outside of work, I enjoy playing soccer,
+              exploring new video games, and traveling to experience different
+              cultures and perspectives.
+            </p>
+            <p className="text-lg mb-4">
+              My goal is to continue growing professionally, learning new
+              technologies, and contributing to innovative projects. I love the
+              idea of working in collaborative teams, where learning and
+              creativity flow freely.
+            </p>
+          </section>
+        </main>
+      </div>
     </div>
   );
 }
