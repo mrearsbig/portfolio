5eb372a5aab38fbca043bb8d35ccba5fc7c278d0
Alexander Viafara <mrearsbig@gmail.com>
2025-01-30 15:51:10 -0500

    Fix revalidate

--- lib/data.ts
+++ lib/data.ts
@@ -1,7 +1,9 @@
 import { Project } from "./definitions";
 
 export async function getProjects(): Promise<Project[]> {
-  const data = await fetch("https://api.github.com/users/mrearsbig/repos");
+  const data = await fetch("https://api.github.com/users/mrearsbig/repos", {
+    next: { revalidate: 60 },
+  });
   const projects: Project[] = await data.json();
 
   const nameProjects = ["portfolio", "nextjs-dashboard", "cashcard"];
